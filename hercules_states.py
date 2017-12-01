f763c7d3e63b92ac8acc5097df76ea23b84cc5d6
Rpi <raviautar27@gmail.com>
2017-12-01 08:46:11 +0000

    Add receive and pass command functionality v1

--- hercules_states.py
+++ hercules_states.py
@@ -1,35 +1,40 @@
-state_commands = {
-        "check_a": [0x01, 0x00],
-        "check_b": [0x02, 0x00],
-        "check_c": [0x03, 0x00],
-        "check_d": [0x04, 0x00],
-        "pump_idle": [0x05, 0x00],
-        "standstil": [0x06, 0x00],
-        "launch": [0x07, 0x00],
-        "service": [0x08, 0x00],
-        "abort": [0x0A, 0x00],
-        "heartbeat": [0x0D, 0xDD]
+PREFIX = [0x0A, 0xAA]
+
+nul = [0x00,0x00]
+tail = [nul for _ in range(5)]
+
+state_transition_commands = {
+        "proceed_check_a": [0x01, 0x00],
+        "proceed_check_b": [0x02, 0x00],
+        "proceed_pump_idle": [0x03, 0x00],
+        "proceed_check_c": [0x04, 0x00],
+        "proceed_standstill": [0x05, 0x00],
+        "launch": [0x06, 0x00],
+        "service_propusion": [0x07, 0x00],
+        "abort": [0x08, 0x00],
+        "proceed_shutdown": [0x09, 0x00],
+        "get_status": [0x0A, 0xAA],
+        "set_target_speed": [0x0B, 0xBB],
+        "set_target_brakepoint": [0x0C, 0xCC]
         }
 
 hercules_states = {
-        '0x100': "idle",
-        '0x200': "check_a",
-        '0x300': "check_b",
-        '0x400': "pump_idle",
-        '0x500': "check_c",
-        '0x600': "Ready",
-        '0x700': "Accelaration",
-        '0x800': "Deceleration",
-        '0x900': "Standstil",
-        '0xA00': "Service propulsion",
-        '0xB00': "System check D",
-        '0xC00': "No communication",
-        '0xD00': "Watchdog expired",
-        '0xE00': "Error"
+        0x0000: "none",
+        0x0100: "idle",
+        0x0200: "check_a",
+        0x0300: "check_b",
+        0x0400: "pumpdown_idle",
+        0x0500: "check_c",
+        0x0600: "ready",
+        0x0700: "acceleration",
+        0x0800: "deceleration",
+        0x0900: "standstill",
+        0x0A00: "service_propulsion",
+        0x0B00: "shutdown",
         }
 
 hercules_sub_states = {
-        '0x000': "None",
-        '0x100': "Busy",
-        '0x200': "Done"
+        0x000: "none",
+        0x100: "busy",
+        0x200: "done"
         }
--- main.py
+++ main.py
@@ -1,41 +1,33 @@
 import paho.mqtt.client as mqtt
 from messenger_ch import mc_messenger
+from messenger_ch import hercules_messenger
 from messenger_ch import udp_messenger
 from commandhandler_ch import command_handler
 import logging
 import time
+import json
 
 def mc_is_alive(last_heartbeat):
     current_time_millis = lambda: time.time()*1000
     heartbeat_exceeded = (current_time_millis() - last_heartbeat) > heartbeat_timeout*1.5
     return False if heartbeat_exceeded else True
 
-"""
-The pod state indicates the state of the pod.
-0: Disconnected: Pod is disconnected and waiting to establish connection with
-                 the mission control.
-1: Connected: Pod is connnected and waiting for commands
-2: Running: The pod is running or braking
-3: Check: The pod is performing system checks/tests
-"""
-pod_state = 0
 heartbeat_timeout = 1500
 
-command_handler = command_handler(pod_state)
 client = mqtt.Client("Communication Hub")
-
-mc_messenger = mc_messenger(client, command_handler, heartbeat_timeout, sending_frequency=1)
+hercules_messenger = hercules_messenger(sending_frequency=1)
+mc_messenger = mc_messenger(client, hercules_messenger, heartbeat_timeout, sending_frequency=1)
 # TODO: spacex_messenger = udp_messenger(client, sending_frequency=2)
-# TODO: hercules_messenger = hercules_messenger()
 
 run = True
 data = "Testing sending."
 
 while run:
+    pod_status = hercules_messenger.get_pod_status()
     if mc_is_alive(mc_messenger.last_heartbeat):
-        mc_messenger.send_data(data)
-    time.sleep(0.2)
-    print("running")
-    # TODO: dma_handler.read(data)
+        mc_messenger.send_data(json.dumps(pod_status))
+    else:
+        hercules_messenger.BRAKE()
+
     # TODO: logger.log(data)
     # TODO: spacex_messenger.send_data(data)
